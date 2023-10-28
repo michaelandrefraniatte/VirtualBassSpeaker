ef1f0e3162665ffeecb338854ecbf4e5e18b6baa
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-10-28 17:42:17 +0200

    Add a link

---
+++ Src/Echorce/Echorce/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Echorce
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "250";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Play";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(62, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "1250";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(205, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Play";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(62, 131);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 22);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "20250";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(331, 201);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Src/Echorce/Echorce/Form1.cs
@@ -0,0 +1,191 @@
+﻿using NAudio.Wave.SampleProviders;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Echorce
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
+        public static extern uint TimeBeginPeriod(uint ms);
+        [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
+        public static extern uint TimeEndPeriod(uint ms);
+        [DllImport("ntdll.dll", EntryPoint = "NtSetTimerResolution")]
+        public static extern void NtSetTimerResolution(uint DesiredResolution, bool SetResolution, ref uint CurrentResolution);
+        public static uint CurrentResolution = 0;
+        private static bool closeonicon = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TimeBeginPeriod(1);
+            NtSetTimerResolution(1, true, ref CurrentResolution);
+            TrayMenuContext();
+            if (System.IO.File.Exists("tempsave"))
+            {
+                using (System.IO.StreamReader file = new System.IO.StreamReader("tempsave"))
+                {
+                    textBox1.Text = file.ReadLine();
+                    textBox2.Text = file.ReadLine();
+                    textBox3.Text = file.ReadLine();
+                }
+            }
+        }
+        private void TrayMenuContext()
+        {
+            this.notifyIcon1.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+            this.notifyIcon1.ContextMenuStrip.Items.Add("Quit", null, this.MenuTest1_Click);
+        }
+        void MenuTest1_Click(object sender, EventArgs e)
+        {
+            closeonicon = true;
+            this.Close();
+        }
+        private void MinimzedTray()
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            this.Hide();
+        }
+        private void MaxmizedFromTray()
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            this.Show();
+        }
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            MaxmizedFromTray();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!closeonicon)
+            {
+                e.Cancel = true;
+                MinimzedTray();
+                return;
+            }
+            using (System.IO.StreamWriter createdfile = new System.IO.StreamWriter("tempsave"))
+            {
+                createdfile.WriteLine(textBox1.Text);
+                createdfile.WriteLine(textBox2.Text);
+                createdfile.WriteLine(textBox3.Text);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Play")
+            {
+                button1.Text = "Stop";
+                Task.Run(() =>
+                {
+                    while (button1.Text == "Stop")
+                    {
+                        var sine20Seconds = new SignalGenerator()
+                        {
+                            Gain = 1,
+                            Frequency = Convert.ToDouble(textBox1.Text) - 250,
+                            FrequencyEnd = Convert.ToDouble(textBox1.Text) + 250,
+                            Type = SignalGeneratorType.Sweep,
+                            SweepLengthSecs = 20
+                        }
+                        .Take(TimeSpan.FromSeconds(20));
+                        using (var wo = new WaveOutEvent())
+                        {
+                            wo.Init(sine20Seconds);
+                            wo.Play();
+                            while (wo.PlaybackState == PlaybackState.Playing)
+                            {
+                                Thread.Sleep(1);
+                            }
+                        }
+                        Thread.Sleep(1);
+                    }
+                });
+            }
+            else
+                button1.Text = "Play";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Play")
+            {
+                button2.Text = "Stop";
+                Task.Run(() =>
+                {
+                    while (button2.Text == "Stop")
+                    {
+                        var sine20Seconds = new SignalGenerator()
+                        {
+                            Gain = 1,
+                            Frequency = Convert.ToDouble(textBox2.Text) - 250,
+                            FrequencyEnd = Convert.ToDouble(textBox2.Text) + 250,
+                            Type = SignalGeneratorType.Sweep,
+                            SweepLengthSecs = 20
+                        }
+                        .Take(TimeSpan.FromSeconds(20));
+                        using (var wo = new WaveOutEvent())
+                        {
+                            wo.Init(sine20Seconds);
+                            wo.Play();
+                            while (wo.PlaybackState == PlaybackState.Playing)
+                            {
+                                Thread.Sleep(1);
+                            }
+                        }
+                        Thread.Sleep(1);
+                    }
+                });
+            }
+            else
+                button2.Text = "Play";
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == "Play")
+            {
+                button3.Text = "Stop";
+                Task.Run(() =>
+                {
+                    while (button3.Text == "Stop")
+                    {
+                        var sine20Seconds = new SignalGenerator()
+                        {
+                            Gain = 1,
+                            Frequency = Convert.ToDouble(textBox3.Text) - 250,
+                            FrequencyEnd = Convert.ToDouble(textBox3.Text) + 250,
+                            Type = SignalGeneratorType.Sweep,
+                            SweepLengthSecs = 20
+                        }
+                        .Take(TimeSpan.FromSeconds(20));
+                        using (var wo = new WaveOutEvent())
+                        {
+                            wo.Init(sine20Seconds);
+                            wo.Play();
+                            while (wo.PlaybackState == PlaybackState.Playing)
+                            {
+                                Thread.Sleep(1);
+                            }
+                        }
+                        Thread.Sleep(1);
+                    }
+                });
+            }
+            else
+                button3.Text = "Play";
+        }
+    }
+}
