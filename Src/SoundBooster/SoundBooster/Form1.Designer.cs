edcfeebdf067b5b6af38d8650ac0d086c79d39f5
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-05-23 13:45:17 +0200

    Add about

--- Src/SoundBooster/SoundBooster/Form1.Designer.cs
+++ Src/SoundBooster/SoundBooster/Form1.Designer.cs
@@ -59,12 +59,13 @@ namespace SoundBooster
             // trackBarEcho
             // 
             this.trackBarEcho.LargeChange = 100;
-            this.trackBarEcho.Location = new System.Drawing.Point(158, 12);
+            this.trackBarEcho.Location = new System.Drawing.Point(211, 15);
+            this.trackBarEcho.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.trackBarEcho.Maximum = 500;
             this.trackBarEcho.Minimum = -500;
             this.trackBarEcho.Name = "trackBarEcho";
             this.trackBarEcho.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBarEcho.Size = new System.Drawing.Size(45, 386);
+            this.trackBarEcho.Size = new System.Drawing.Size(56, 475);
             this.trackBarEcho.SmallChange = 10;
             this.trackBarEcho.TabIndex = 0;
             this.trackBarEcho.TickFrequency = 10;
@@ -74,9 +75,10 @@ namespace SoundBooster
             // checkBoxEcho
             // 
             this.checkBoxEcho.AutoSize = true;
-            this.checkBoxEcho.Location = new System.Drawing.Point(158, 404);
+            this.checkBoxEcho.Location = new System.Drawing.Point(211, 497);
+            this.checkBoxEcho.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxEcho.Name = "checkBoxEcho";
-            this.checkBoxEcho.Size = new System.Drawing.Size(50, 17);
+            this.checkBoxEcho.Size = new System.Drawing.Size(59, 20);
             this.checkBoxEcho.TabIndex = 1;
             this.checkBoxEcho.Text = "echo";
             this.checkBoxEcho.UseVisualStyleBackColor = true;
@@ -85,12 +87,13 @@ namespace SoundBooster
             // trackBarReverb
             // 
             this.trackBarReverb.LargeChange = 100;
-            this.trackBarReverb.Location = new System.Drawing.Point(275, 12);
+            this.trackBarReverb.Location = new System.Drawing.Point(367, 15);
+            this.trackBarReverb.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.trackBarReverb.Maximum = 500;
             this.trackBarReverb.Minimum = -500;
             this.trackBarReverb.Name = "trackBarReverb";
             this.trackBarReverb.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBarReverb.Size = new System.Drawing.Size(45, 386);
+            this.trackBarReverb.Size = new System.Drawing.Size(56, 475);
             this.trackBarReverb.SmallChange = 10;
             this.trackBarReverb.TabIndex = 2;
             this.trackBarReverb.TickFrequency = 10;
@@ -100,12 +103,13 @@ namespace SoundBooster
             // trackBarFlanger
             // 
             this.trackBarFlanger.LargeChange = 100;
-            this.trackBarFlanger.Location = new System.Drawing.Point(389, 12);
+            this.trackBarFlanger.Location = new System.Drawing.Point(519, 15);
+            this.trackBarFlanger.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.trackBarFlanger.Maximum = 500;
             this.trackBarFlanger.Minimum = -500;
             this.trackBarFlanger.Name = "trackBarFlanger";
             this.trackBarFlanger.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBarFlanger.Size = new System.Drawing.Size(45, 386);
+            this.trackBarFlanger.Size = new System.Drawing.Size(56, 475);
             this.trackBarFlanger.SmallChange = 10;
             this.trackBarFlanger.TabIndex = 5;
             this.trackBarFlanger.TickFrequency = 10;
@@ -115,12 +119,13 @@ namespace SoundBooster
             // trackBarGargle
             // 
             this.trackBarGargle.LargeChange = 100;
-            this.trackBarGargle.Location = new System.Drawing.Point(512, 12);
+            this.trackBarGargle.Location = new System.Drawing.Point(683, 15);
+            this.trackBarGargle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.trackBarGargle.Maximum = 500;
             this.trackBarGargle.Minimum = -500;
             this.trackBarGargle.Name = "trackBarGargle";
             this.trackBarGargle.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBarGargle.Size = new System.Drawing.Size(45, 386);
+            this.trackBarGargle.Size = new System.Drawing.Size(56, 475);
             this.trackBarGargle.SmallChange = 10;
             this.trackBarGargle.TabIndex = 8;
             this.trackBarGargle.TickFrequency = 10;
@@ -130,9 +135,10 @@ namespace SoundBooster
             // checkBoxReverb
             // 
             this.checkBoxReverb.AutoSize = true;
-            this.checkBoxReverb.Location = new System.Drawing.Point(264, 404);
+            this.checkBoxReverb.Location = new System.Drawing.Point(352, 497);
+            this.checkBoxReverb.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxReverb.Name = "checkBoxReverb";
-            this.checkBoxReverb.Size = new System.Drawing.Size(56, 17);
+            this.checkBoxReverb.Size = new System.Drawing.Size(68, 20);
             this.checkBoxReverb.TabIndex = 26;
             this.checkBoxReverb.Text = "reverb";
             this.checkBoxReverb.UseVisualStyleBackColor = true;
@@ -141,12 +147,13 @@ namespace SoundBooster
             // trackBarNormal
             // 
             this.trackBarNormal.LargeChange = 100;
-            this.trackBarNormal.Location = new System.Drawing.Point(44, 12);
+            this.trackBarNormal.Location = new System.Drawing.Point(59, 15);
+            this.trackBarNormal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.trackBarNormal.Maximum = 500;
             this.trackBarNormal.Minimum = -500;
             this.trackBarNormal.Name = "trackBarNormal";
             this.trackBarNormal.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBarNormal.Size = new System.Drawing.Size(45, 386);
+            this.trackBarNormal.Size = new System.Drawing.Size(56, 475);
             this.trackBarNormal.SmallChange = 10;
             this.trackBarNormal.TabIndex = 29;
             this.trackBarNormal.TickFrequency = 10;
@@ -156,9 +163,10 @@ namespace SoundBooster
             // checkBoxNormal
             // 
             this.checkBoxNormal.AutoSize = true;
-            this.checkBoxNormal.Location = new System.Drawing.Point(44, 404);
+            this.checkBoxNormal.Location = new System.Drawing.Point(59, 497);
+            this.checkBoxNormal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxNormal.Name = "checkBoxNormal";
-            this.checkBoxNormal.Size = new System.Drawing.Size(57, 17);
+            this.checkBoxNormal.Size = new System.Drawing.Size(70, 20);
             this.checkBoxNormal.TabIndex = 30;
             this.checkBoxNormal.Text = "normal";
             this.checkBoxNormal.UseVisualStyleBackColor = true;
@@ -167,9 +175,10 @@ namespace SoundBooster
             // checkBoxFlanger
             // 
             this.checkBoxFlanger.AutoSize = true;
-            this.checkBoxFlanger.Location = new System.Drawing.Point(376, 404);
+            this.checkBoxFlanger.Location = new System.Drawing.Point(501, 497);
+            this.checkBoxFlanger.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxFlanger.Name = "checkBoxFlanger";
-            this.checkBoxFlanger.Size = new System.Drawing.Size(58, 17);
+            this.checkBoxFlanger.Size = new System.Drawing.Size(70, 20);
             this.checkBoxFlanger.TabIndex = 31;
             this.checkBoxFlanger.Text = "flanger";
             this.checkBoxFlanger.UseVisualStyleBackColor = true;
@@ -178,9 +187,10 @@ namespace SoundBooster
             // checkBoxGargle
             // 
             this.checkBoxGargle.AutoSize = true;
-            this.checkBoxGargle.Location = new System.Drawing.Point(512, 404);
+            this.checkBoxGargle.Location = new System.Drawing.Point(683, 497);
+            this.checkBoxGargle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxGargle.Name = "checkBoxGargle";
-            this.checkBoxGargle.Size = new System.Drawing.Size(55, 17);
+            this.checkBoxGargle.Size = new System.Drawing.Size(68, 20);
             this.checkBoxGargle.TabIndex = 32;
             this.checkBoxGargle.Text = "gargle";
             this.checkBoxGargle.UseVisualStyleBackColor = true;
@@ -189,12 +199,13 @@ namespace SoundBooster
             // trackBarDistortion
             // 
             this.trackBarDistortion.LargeChange = 100;
-            this.trackBarDistortion.Location = new System.Drawing.Point(640, 12);
+            this.trackBarDistortion.Location = new System.Drawing.Point(853, 15);
+            this.trackBarDistortion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.trackBarDistortion.Maximum = 500;
             this.trackBarDistortion.Minimum = -500;
             this.trackBarDistortion.Name = "trackBarDistortion";
             this.trackBarDistortion.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBarDistortion.Size = new System.Drawing.Size(45, 386);
+            this.trackBarDistortion.Size = new System.Drawing.Size(56, 475);
             this.trackBarDistortion.SmallChange = 10;
             this.trackBarDistortion.TabIndex = 33;
             this.trackBarDistortion.TickFrequency = 10;
@@ -204,12 +215,13 @@ namespace SoundBooster
             // trackBarCompressor
             // 
             this.trackBarCompressor.LargeChange = 100;
-            this.trackBarCompressor.Location = new System.Drawing.Point(768, 12);
+            this.trackBarCompressor.Location = new System.Drawing.Point(1024, 15);
+            this.trackBarCompressor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.trackBarCompressor.Maximum = 500;
             this.trackBarCompressor.Minimum = -500;
             this.trackBarCompressor.Name = "trackBarCompressor";
             this.trackBarCompressor.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBarCompressor.Size = new System.Drawing.Size(45, 386);
+            this.trackBarCompressor.Size = new System.Drawing.Size(56, 475);
             this.trackBarCompressor.SmallChange = 10;
             this.trackBarCompressor.TabIndex = 34;
             this.trackBarCompressor.TickFrequency = 10;
@@ -219,12 +231,13 @@ namespace SoundBooster
             // trackBarChorus
             // 
             this.trackBarChorus.LargeChange = 100;
-            this.trackBarChorus.Location = new System.Drawing.Point(891, 12);
+            this.trackBarChorus.Location = new System.Drawing.Point(1188, 15);
+            this.trackBarChorus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.trackBarChorus.Maximum = 500;
             this.trackBarChorus.Minimum = -500;
             this.trackBarChorus.Name = "trackBarChorus";
             this.trackBarChorus.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBarChorus.Size = new System.Drawing.Size(45, 386);
+            this.trackBarChorus.Size = new System.Drawing.Size(56, 475);
             this.trackBarChorus.SmallChange = 10;
             this.trackBarChorus.TabIndex = 35;
             this.trackBarChorus.TickFrequency = 10;
@@ -234,9 +247,10 @@ namespace SoundBooster
             // checkBoxDistortion
             // 
             this.checkBoxDistortion.AutoSize = true;
-            this.checkBoxDistortion.Location = new System.Drawing.Point(630, 404);
+            this.checkBoxDistortion.Location = new System.Drawing.Point(840, 497);
+            this.checkBoxDistortion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxDistortion.Name = "checkBoxDistortion";
-            this.checkBoxDistortion.Size = new System.Drawing.Size(68, 17);
+            this.checkBoxDistortion.Size = new System.Drawing.Size(83, 20);
             this.checkBoxDistortion.TabIndex = 36;
             this.checkBoxDistortion.Text = "distortion";
             this.checkBoxDistortion.UseVisualStyleBackColor = true;
@@ -245,9 +259,10 @@ namespace SoundBooster
             // checkBoxCompressor
             // 
             this.checkBoxCompressor.AutoSize = true;
-            this.checkBoxCompressor.Location = new System.Drawing.Point(746, 404);
+            this.checkBoxCompressor.Location = new System.Drawing.Point(995, 497);
+            this.checkBoxCompressor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxCompressor.Name = "checkBoxCompressor";
-            this.checkBoxCompressor.Size = new System.Drawing.Size(80, 17);
+            this.checkBoxCompressor.Size = new System.Drawing.Size(101, 20);
             this.checkBoxCompressor.TabIndex = 37;
             this.checkBoxCompressor.Text = "compressor";
             this.checkBoxCompressor.UseVisualStyleBackColor = true;
@@ -256,9 +271,10 @@ namespace SoundBooster
             // checkBoxChorus
             // 
             this.checkBoxChorus.AutoSize = true;
-            this.checkBoxChorus.Location = new System.Drawing.Point(883, 404);
+            this.checkBoxChorus.Location = new System.Drawing.Point(1177, 497);
+            this.checkBoxChorus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxChorus.Name = "checkBoxChorus";
-            this.checkBoxChorus.Size = new System.Drawing.Size(58, 17);
+            this.checkBoxChorus.Size = new System.Drawing.Size(69, 20);
             this.checkBoxChorus.TabIndex = 38;
             this.checkBoxChorus.Text = "chorus";
             this.checkBoxChorus.UseVisualStyleBackColor = true;
@@ -266,9 +282,9 @@ namespace SoundBooster
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(979, 433);
+            this.ClientSize = new System.Drawing.Size(1305, 533);
             this.Controls.Add(this.checkBoxChorus);
             this.Controls.Add(this.checkBoxCompressor);
             this.Controls.Add(this.checkBoxDistortion);
@@ -286,11 +302,13 @@ namespace SoundBooster
             this.Controls.Add(this.checkBoxEcho);
             this.Controls.Add(this.trackBarEcho);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SoundBooster";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.trackBarEcho)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBarReverb)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBarFlanger)).EndInit();
